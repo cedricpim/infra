e8847c261f480f2aa1937524c16ffb59d97882c4
Cedric Pimenta <cedricpim@users.noreply.github.com>
2024-06-01 00:42:12 +0100

    Add User example table

--- db/migrate/20240531232856_add_user_table.rb
+++ db/migrate/20240531232856_add_user_table.rb
@@ -1,6 +1,6 @@
 class AddUserTable < ActiveRecord::Migration[7.1]
   def change
-    create_table :users, :uuid do |t|
+    create_table :users, id: :uuid do |t|
       t.string :name
       t.timestamps
 
